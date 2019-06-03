396a3596e808d4bdfc5c481c185d512239762ff1
James Barash <jbarash@meritdirect.com>
2019-06-03 09:32:18 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Start = New System.Windows.Forms.Button()
+        Me.lblRowCount = New System.Windows.Forms.Label()
+        Me.btnSelectFile = New System.Windows.Forms.Button()
+        Me.txtRowCount = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblOutputFile = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.lblFileName = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Start
+        '
+        Me.Start.Location = New System.Drawing.Point(136, 184)
+        Me.Start.Name = "Start"
+        Me.Start.Size = New System.Drawing.Size(75, 23)
+        Me.Start.TabIndex = 0
+        Me.Start.Text = "Run"
+        Me.Start.UseVisualStyleBackColor = True
+        '
+        'lblRowCount
+        '
+        Me.lblRowCount.AutoSize = True
+        Me.lblRowCount.Location = New System.Drawing.Point(133, 256)
+        Me.lblRowCount.Name = "lblRowCount"
+        Me.lblRowCount.Size = New System.Drawing.Size(10, 13)
+        Me.lblRowCount.TabIndex = 1
+        Me.lblRowCount.Text = "."
+        '
+        'btnSelectFile
+        '
+        Me.btnSelectFile.Location = New System.Drawing.Point(136, 32)
+        Me.btnSelectFile.Name = "btnSelectFile"
+        Me.btnSelectFile.Size = New System.Drawing.Size(75, 23)
+        Me.btnSelectFile.TabIndex = 2
+        Me.btnSelectFile.Text = "Select File"
+        Me.btnSelectFile.UseVisualStyleBackColor = True
+        '
+        'txtRowCount
+        '
+        Me.txtRowCount.Location = New System.Drawing.Point(136, 101)
+        Me.txtRowCount.Name = "txtRowCount"
+        Me.txtRowCount.Size = New System.Drawing.Size(123, 20)
+        Me.txtRowCount.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(136, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 4
+        '
+        'lblOutputFile
+        '
+        Me.lblOutputFile.AutoSize = True
+        Me.lblOutputFile.Location = New System.Drawing.Point(136, 220)
+        Me.lblOutputFile.Name = "lblOutputFile"
+        Me.lblOutputFile.Size = New System.Drawing.Size(10, 13)
+        Me.lblOutputFile.TabIndex = 6
+        Me.lblOutputFile.Text = "."
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(40, 108)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Max Row Per File"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(139, 137)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(120, 17)
+        Me.CheckBox1.TabIndex = 8
+        Me.CheckBox1.Text = "Header In Each File"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'lblFileName
+        '
+        Me.lblFileName.AutoSize = True
+        Me.lblFileName.Location = New System.Drawing.Point(133, 70)
+        Me.lblFileName.Name = "lblFileName"
+        Me.lblFileName.Size = New System.Drawing.Size(10, 13)
+        Me.lblFileName.TabIndex = 9
+        Me.lblFileName.Text = "."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1040, 450)
+        Me.Controls.Add(Me.lblFileName)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblOutputFile)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtRowCount)
+        Me.Controls.Add(Me.btnSelectFile)
+        Me.Controls.Add(Me.lblRowCount)
+        Me.Controls.Add(Me.Start)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "SplitFile"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Start As Button
+    Friend WithEvents lblRowCount As Label
+    Friend WithEvents btnSelectFile As Button
+    Friend WithEvents txtRowCount As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblOutputFile As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents lblFileName As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,78 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim txtFileName As String = ""
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Start.Click
+        Dim line As String, cnt As Integer, fileno As Integer, header As String = String.Empty
+        Dim destStream As StreamWriter
+        Try
+            cnt = 0
+            fileno = 1
+            If txtFileName.Length < 1 Then
+                MessageBox.Show("You must Select a file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                Exit Sub
+            End If
+            If Not IsNumeric(txtRowCount.Text) OrElse CInt(txtRowCount.Text) < 1 Then
+                MessageBox.Show("You must enter a Row Count", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                Exit Sub
+            End If
+            Dim txtFilePre = Path.GetFileNameWithoutExtension(txtFileName)
+            Dim txtExt = Path.GetExtension(txtFileName)
+            Dim txtFilePath = Path.GetDirectoryName(txtFileName)
+            destStream = New StreamWriter(txtFilePath & "\" & txtFilePre & "-" & fileno.ToString() & txtExt)
+            lblOutputFile.Text = txtFilePre & "-" & fileno.ToString() & txtExt
+            Using sourceStream As StreamReader = New StreamReader(txtFileName)
+                If CheckBox1.Checked Then
+                    header = sourceStream.ReadLine
+                    destStream.WriteLine(header)
+                    cnt += 1
+                End If
+                While sourceStream.Peek() >= 0
+                    line = sourceStream.ReadLine
+                    If cnt >= CInt(txtRowCount.Text) Then
+                        destStream.Close()
+                        destStream = Nothing
+                        fileno += 1
+                        destStream = New StreamWriter(txtFilePath & "\" & txtFilePre & "-" & fileno.ToString() & txtExt)
+                        lblOutputFile.Text = txtFilePre & "-" & fileno.ToString() & txtExt
+                        lblOutputFile.Refresh()
+                        If CheckBox1.Checked Then 'header
+                            destStream.WriteLine(header)
+                            cnt = 1
+                        Else
+                            cnt = 0 ' no header
+                        End If
+
+                    End If
+                    destStream.WriteLine(line)
+                    cnt += 1
+                    If cnt Mod 1000 = 0 Then
+                        lblRowCount.Text = cnt.ToString()
+                        lblRowCount.Refresh()
+                    End If
+                End While
+            End Using
+            destStream.Close()
+            destStream = Nothing
+            MessageBox.Show("Done")
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
+
+    End Sub
+
+    Private Sub btnSelectFile_Click(sender As Object, e As EventArgs) Handles btnSelectFile.Click
+        Dim fd As OpenFileDialog = New OpenFileDialog()
+        fd.Title = "Select File to Split"
+        fd.InitialDirectory = "C:\"
+        fd.Filter = "Text file|*.txt;*.csv|All files (*.*)|*.*"
+        fd.FilterIndex = 1
+        fd.RestoreDirectory = True
+        If fd.ShowDialog() = DialogResult.OK Then
+            txtFileName = fd.FileName
+            lblFileName.Text = txtFileName
+        End If
+    End Sub
+
+
+End Class
